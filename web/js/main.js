7d2435b1e7158c9118d07399d9652970d7b767c2
andy <andersgee@gmail.com>
2020-05-12 18:06:29 +0200

    3 buttons

--- web/js/main.js
+++ web/js/main.js
@@ -15,9 +15,18 @@ function setup() {
   canvas.width = window.innerWidth * 0.9;
   canvas.height = 300;
 
-  document.getElementById("but1").addEventListener("click", change_data(1));
-  document.getElementById("but2").addEventListener("click", change_data(2));
-  document.getElementById("but3").addEventListener("click", change_data(3));
+  let b1 = document.getElementById("but1");
+  let b2 = document.getElementById("but2");
+  let b3 = document.getElementById("but3");
+  b1.addEventListener("click", () => {
+    change_data(1);
+  });
+  b2.addEventListener("click", () => {
+    change_data(2);
+  });
+  b3.addEventListener("click", () => {
+    change_data(3);
+  });
 
   var plotarguments = {
     time_axis: { first_year: 1999, last_year: 2059 }, //plot x axis
@@ -45,11 +54,41 @@ function setup() {
     //convert to attributes for glsl
     atr = plotattributes(plotarguments, canvas, emissions_sum, budget_sum);
     vao = vertexarray(shaders.dotplot, atr);
+
     main();
   });
 }
 function change_data(i) {
   console.log(i);
+  if (i == 1) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p1);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p1), gl.DYNAMIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p2);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p2), gl.DYNAMIC_DRAW);
+    uniforms.t = 0;
+  } else if (i == 2) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p1);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p2), gl.DYNAMIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p2);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p1), gl.DYNAMIC_DRAW);
+    uniforms.t = 0;
+  } else if (i == 3) {
+    let newp = atr.p1.slice(0);
+    newp[4] = 30;
+    newp[5] = 30;
+    newp[6] = 20;
+    newp[7] = 15;
+
+    //update plotattributes like so:
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p1);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p1), gl.DYNAMIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p2);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(newp), gl.DYNAMIC_DRAW);
+    uniforms.t = 0;
+  }
 }
 
 function main() {
@@ -57,15 +96,6 @@ function main() {
 }
 
 function renderplot() {
-  //change some data, for debugging
-  atr.p2[4] += 0.1;
-  atr.p2[5] += 0.1;
-  atr.p2[6] += 0.1;
-  atr.p2[7] += 0.05;
-  //update plotattributes like so:
-  gl.bindBuffer(gl.ARRAY_BUFFER, atrbuffers.p2);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(atr.p2), gl.DYNAMIC_DRAW);
-
   //gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   //updateuniforms();
   uniforms.t = Math.min(uniforms.t + 0.1, 22);
